fb96228f16793ca1deab9a0aa0f80493bd5d69b4
alejandramolin <74629155+alejandramolin@users.noreply.github.com>
2020-11-20 19:04:05 -0500

    Hola

---
+++ data/Filiales_construccion_3.js
@@ -0,0 +1,10 @@
+var json_Filiales_construccion_3 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID_ESPACIA": "15001", "ENTIDAD_TE": "CAP. DEPTO", "NOM_DEPART": "BOYACÁ", "NOM_MUNICI": "TUNJA", "ID_VISITA": "777381", "ID_MAPA": "1", "FILIAL": "Y1", "nom_filial": "Gas Natural Cundiboyacense" }, "geometry": { "type": "Point", "coordinates": [ -73.3779, 5.5175 ] } },
+{ "type": "Feature", "properties": { "ID_ESPACIA": "11001", "ENTIDAD_TE": "CAP. DEPTO Distrito Capital", "NOM_DEPART": "SATA FE DE BOGOTÁ D.C.", "NOM_MUNICI": "BOGOTÁ, D.C.", "ID_VISITA": "777381", "ID_MAPA": "1", "FILIAL": "C1", "nom_filial": "Vanti" }, "geometry": { "type": "Point", "coordinates": [ -74.1818, 4.3123 ] } },
+{ "type": "Feature", "properties": { "ID_ESPACIA": "20011", "ENTIDAD_TE": "Cabecera Municipal", "NOM_DEPART": "CESAR", "NOM_MUNICI": "AGUACHICA", "ID_VISITA": "777381", "ID_MAPA": "1", "FILIAL": "N1", "nom_filial": "Gas Nacer" }, "geometry": { "type": "Point", "coordinates": [ -73.6214, 8.2498 ] } },
+{ "type": "Feature", "properties": { "ID_ESPACIA": "68001", "ENTIDAD_TE": "CAP. DEPTO Distrito Histórico y Cultural", "NOM_DEPART": "SANTANDER", "NOM_MUNICI": "BUCARAMANGA", "ID_VISITA": "777381", "ID_MAPA": "1", "FILIAL": "O1", "nom_filial": "Gas oriente" }, "geometry": { "type": "Point", "coordinates": [ -73.1113, 7.1555 ] } }
+]
+}
